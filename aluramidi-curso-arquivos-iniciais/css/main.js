3bbdb24080350b05296ba2d07fa7e2ce4fd2d893
larissa vitoria mentges <107115194+lari12345@users.noreply.github.com>
2023-10-04 12:22:46 -0300

    Add files via upload

--- aluramidi-curso-arquivos-iniciais/css/main.js
+++ aluramidi-curso-arquivos-iniciais/css/main.js
@@ -1,9 +1,32 @@
+//Pom -------------------------------------
+
 function tocaSomPom(){
     document.querySelector('#som_tecla_pom').play();
 }
-
-
-
-
 document.querySelector('.tecla_pom').onclick=tocaSomPom;
+
+document.querySelectorAll('.tecla')
+
+ const listaDeTeclas = document.querySelectorAll('.tecla')
+
+    listaDeTeclas[1] .onclick = tocaSomClap;
+    listaDeTeclas[2] .onclick = tocaSomTim;
+    listaDeTeclas[3] .onclick = tocaSomPuff;
+    listaDeTeclas[4] .onclick = tocaSomSplash;
+    listaDeTeclas[5] .onclick = tocaSomToim;
+    listaDeTeclas[6] .onclick = tocaSomPsh;
+    listaDeTeclas[7] .onclick = tocaSomTim;
+    listaDeTeclas[8] .onclick = tocaSomTom;
+
+
+
+
+
+
+
+
+
+
+
+
 
